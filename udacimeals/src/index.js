88009d15fe722c49e99b44a2569adb45013c4f5b
Chris Dunkel <chris@careerfairplus.com>
2018-03-26 14:48:19 -0400

    Initial commit.
    Includes:
    - Basic React app.
    - Basic Redux implementation.

---
+++ udacimeals/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './index.css';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(
+  reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+
+ReactDOM.render(
+  <App store={store}/>,
+  document.getElementById('root')
+);
